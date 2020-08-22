16ca3515be4161c6ef025bade5f8df3bce386767
Josh Batchelor <josh_batch@live.com.au>
2020-08-22 11:20:41 +1000

    Adds client sockets helper module

---
+++ src/client/sockets/sockets.ts
@@ -0,0 +1,38 @@
+import socketIO from 'socket.io-client';
+
+// our single instance of socket that everyone will use
+let socket: SocketIOClient.Socket;
+
+function getSocket() {
+  if (!socket) {
+    throw Error('Attempted to load socket before it was initialised');
+  }
+  return socket;
+}
+
+function initialiseSocket() {
+  socket = socketIO.connect(`ws://${window.location.host}`);
+  socket.on('connect', () => {
+    console.log('Connected to server')
+  });
+
+  return socket;
+}
+
+function safeEmit<Event extends keyof SocketEvents>(
+  event: Event,
+  payload?: SocketEvents[Event]
+) {
+  getSocket().emit(event, payload);
+}
+function safeOn<Event extends keyof SocketEvents>(
+  event: Event,
+  callback: (payload?: SocketEvents[Event]) => void
+) {
+  getSocket().on(event, callback);
+}
+function safeOff<Event extends keyof SocketEvents>(event: Event) {
+  getSocket().off(event);
+}
+
+export { initialiseSocket, getSocket, safeEmit, safeOn, safeOff };
